ebb8249613acdebc9d4f2e3ce89bd0fa52fe6fd3
Ryan Seymore <rjohnsondev@protonmail.com>
2020-08-03 20:33:11 -0600

    implementing hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/logo.svg";
 import NavList from "../components/NavList";
 import MobileNav from "../components/MobileNav";
 
 export default function Header({ setActive, active }) {
+  const [menu, setMenu] = useState(false);
+
+  const setMenuDelay = (state) => {
+    setTimeout(() => setMenu(state), 400)
+  }
+
   return (
-    <header className="header">
-      <div
-        role="button"
-        tabIndex={0}
-        className="brand-container"
-        onKeyDown={() => setActive(0)}
-        onClick={() => setActive(0)}
-      >
-        <img src={Logo} className="logo" alt="Ryan Johnson - Developer" />
-        <div className="name-container">
-          <h3>Ryan Johnson</h3>
-          <h4>Developer</h4>
+    <>
+      <header className="header">
+        <div
+          role="button"
+          tabIndex={0}
+          className="brand-container"
+          onKeyDown={() => setActive(0)}
+          onClick={() => setActive(0)}
+        >
+          <img src={Logo} className="logo" alt="Ryan Johnson - Developer" />
+          <div className="name-container">
+            <h3>Ryan Johnson</h3>
+            <h4>Developer</h4>
+          </div>
         </div>
+        <NavList setActive={setActive} active={active} />
+        <MobileNav setMenu={setMenu} menu={menu} />
+      </header>
+      <div className={`menu-transition ${menu ? "open" : ""}`}>
+        <ul className="nav-mobile-menu">
+          <li>
+            <a
+              onClick={() => {
+                setActive(0)
+                setMenuDelay(false)
+                }
+              }
+              onKeyDown={() => setActive(0)}
+            >
+              Work
+            </a>
+          </li>
+          <li>
+            <a
+              onClick={() => {
+                setActive(1)
+                setMenuDelay(false)
+                }
+              }
+              onKeyDown={() => setActive(2)}
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              onClick={() => {
+                setActive(2)
+                setMenuDelay(false)
+                }
+              }
+              onKeyDown={() => setActive(1)}
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
       </div>
-      <NavList setActive={setActive} active={active} />
-      <MobileNav />
-    </header>
+    </>
   );
 }
--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,37 +1,21 @@
 import React from "react";
 // import { Link } from "react-scroll";
 
-class MobileNav extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      menuOpen: false,
-    };
-  }
-
-  handleStateChange() {
-    this.setState({ menuOpen: !this.state.menuOpen });
-  }
-
-  render() {
-    return (
-      <nav className="nav-mobile">
-        <div
-          id="nav-icon3"
-          role="button"
-          onClick={() => this.handleStateChange()}
-          onKeyDown={() => this.handleStateChange()}
-          tabIndex={0}
-          className={`${this.state.menuOpen ? "open" : ""}`}
-        >
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </nav>
-    );
-  }
+export default function MobileNav({ setMenu }) {
+  return (
+    <nav onClick={setMenu} className="nav-mobile-icon">
+      <div
+        id="nav-icon3"
+        role="button"
+        onClick={() => setMenu(true)}
+        onKeyDown={() => setMenu(true)}
+        tabIndex={0}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </nav>
+  );
 }
-
-export default MobileNav;
