0487307d4fdb5bba63b6771b3d0436c75f336aee
rjohnson91 <ryanmjohnson@protonmail.com>
2021-03-23 19:05:00 -0600

    adding contact page UI

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,35 +1,107 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { CheckCircleOutlined, CloseCircleOutlined } from "@ant-design/icons";
 
 export default React.memo(function Contact() {
+  const [formValues, setFormValues] = useState({});
+  const [message, setMessage] = useState({
+    status: "",
+    message: "",
+    active: false,
+  });
+  const [sending, setSending] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  useEffect(() => {
+    if (message && message.active) {
+      setTimeout(() => setMessage({ ...message, active: false }), 10000);
+    }
+  }, [message]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    const requiredFields = ["fname", "lname", "email", "message"];
+    for (const requiredField of requiredFields) {
+      if (!formValues[requiredField]) return;
+    }
+    setSending(true);
+    setTimeout(() => {
+      setSending(false);
+      setFormValues({});
+      setMessage({ status: "success", message: "Message sent!", active: true });
+    }, 3000);
+  };
+
+  const renderMessage = () => {
+    return (
+      <div
+        className={`contact-form-message ${message.status} ${
+          message && message.active && "active"
+        }`}
+      >
+        {message.status === "success" ? (
+          <CheckCircleOutlined />
+        ) : (
+          <CloseCircleOutlined />
+        )}
+        <p>{message.message}</p>
+      </div>
+    );
+  };
+
   return (
     <section className="contact">
       <section className="sub-header">
         <h2>Get in Touch!</h2>
       </section>
-      <form className="contact-form" onSubmit={(e) => e.preventDefault()}>
+      <form className="contact-form" onSubmit={sendMessage}>
         <div className="contact-name">
           <div className="contact-name-first">
             <label htmlFor="fname">First name</label>
-            <input type="text" id="fname" name="fname" />
+            <input
+              type="text"
+              name="fname"
+              onChange={handleChange}
+              required
+              minLength="2"
+              value={formValues["fname"] || ""}
+            />
           </div>
           <div className="contact-name-last">
             <label htmlFor="fname">Last name</label>
-            <input type="text" id="lname" name="lname" />
+            <input
+              type="text"
+              name="lname"
+              onChange={handleChange}
+              required
+              minLength="2"
+              value={formValues["lname"] || ""}
+            />
           </div>
         </div>
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" />
+        <input
+          type="email"
+          name="email"
+          onChange={handleChange}
+          required
+          value={formValues["email"] || ""}
+        />
         <label htmlFor="subject">Message</label>
         <textarea
-          name="comment[body]"
+          name="message"
           rows="1"
           cols="50"
           wrap="physical"
-          id="message"
+          onChange={handleChange}
+          required
+          value={formValues["message"] || ""}
         />
-        <button type="submit" value="Submit">
-          Send
-        </button>
+
+        <button type="submit">{sending ? "Sending..." : "Send"}</button>
       </form>
       <footer className="contact-footer">
         <p>
@@ -39,6 +111,7 @@ export default React.memo(function Contact() {
           </a>
         </p>
       </footer>
+      {renderMessage()}
     </section>
   );
 });
