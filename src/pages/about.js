629c6c4246b76e167947515e0963ca3bde9736d4
rjohnson91 <ryanmjohnson@protonmail.com>
2021-03-31 14:22:32 -0600

    style updates, adding resume, updating about

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,16 +12,17 @@ export default React.memo(function About() {
           <h2>A little about me</h2>
         </section>
         <p>
-          I&apos;m a full-stack developer based in Boise, Idaho. I’m obsessed
-          with technology and enjoy creating applications that solve real-world
-          problems. I’ve worked in a variety of roles with teams large and small
-          to build awesome experiences. Outside of work, I enjoy reading,
-          watching documentaries and learning new technologies.
+          I&apos;m a full-stack developer based in Boise, Idaho. I&apos;ve
+          always had an interest for technology and the problems it can solve.
+          I’ve had a variety of roles in tech design and development, working
+          with teams large and small to build awesome experiences. Outside of
+          work, I enjoy reading, running, and playing role-playing games.
         </p>
         <p>
-          I always enjoy meeting new people and exploring opportunities. If you
-          have a project that you want to get started, think you need my help
-          with something, or just fancy saying hey — then email me at{" "}
+          I enjoy meeting new people and exploring opportunities. If you have a
+          project that you want to get started, think you need my help with
+          something, or just fancy saying hey — then don&apos;t hesitate to
+          email me at{" "}
           <a href="mailto:rjohnsondev@protonmail.com" alt="get-in-touch">
             rjohnsondev@protonmail.com
           </a>
